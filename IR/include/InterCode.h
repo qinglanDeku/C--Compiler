e6e3adf76df9c2b346aee6f69acfa06b8a9a65b9
qinglanDeku <771443587@qq.com>
2018-12-05 21:52:47 +0800

    InterCode v1

--- IR/include/InterCode.h
+++ IR/include/InterCode.h
@@ -6,17 +6,21 @@ using std::list;
 
 class Operand{
 public:
-    enum OPType {VARIABLE, TEMPVARIABLE, CONSTANT, ADRESS, TEMPADRESS} ;      
-    Operand(OPType type, int No):type(type), No(No){}
+
+    enum OPType {VARIABLE, TEMPVARIABLE, ICONSTANT, FCONSTANT, ADRESS, TEMPADRESS} ;      
+    Operand(OPType type, int No):type(type){
+        this->No = No;
+        name = produceName();}
     Operand(){}
     ~Operand(){}
     const OPType getType(){return type;}
-    const string getName();
+    const string getName(){return name;}
     void setValue(int Ival){value.IVal = Ival;}
     void setValue(float Fval){value.FVal = Fval;}
     
 
 private:
+    static int tempVarNum;
     OPType type;
     union{
         int IVal;       //int val of a variable/constant
@@ -24,26 +28,181 @@ private:
         int adress;     //adress number of an adress
         
     }value;
-    int No;         //number of varibale or temporary varaible
+    int No;         //number of varibale
+
+    string name;
+    string produceName();
 
 };
 
 class InterCode{
 public:
-    enum IRType{ASSIGN, PLUS, SUB, MUL, DIV, MINUS, FUNC, LABEL, 
+    enum IRtype{ASSIGN, PLUS, SUB, MUL, DIV, FUNC, LABEL, 
     ASSIGNLOC,      // code like "a = &b;"
     ASSIGNFROMLOC,  //code like "a = *b;"
     ASSIGNTOLOC, //code like "*a = b;"
-    GOTO, IFGOTO, DEC, //this for definition/declaration of an array or struct
+    GOTO, COND, RETURN,
+    DEC, //this for definition/declaration of an array or struct
     ARG, CALL, PARAM, READ, WRITE};
+    InterCode(IRtype type):type(type){}
+    const IRtype getType(){
+        return type;
+    }
+    const string getCode(){return code;}
+    
 
+protected:
+    IRtype type;
+    string code;
+};
+
+class AssignCode:public InterCode{
+public:
+    AssignCode(IRtype type, Operand *left, Operand *right);
+    const Operand getLeftOp(){return left;}
+    const Operand getRightOp(){return right;}
+private:
+    Operand left, right; 
+    string produceCode();
+};
+
+class BinopCode:public InterCode{
+public:
+    BinopCode(IRtype type, Operand *result, Operand *op1, Operand *op2);
+    const Operand getResult(){return result;}
+    const Operand getOp1(){return op1;}
+    const Operand getOp2(){return op2;}
 
 private:
-    IRType type;
-    union{
-        struct{Operand left, right;} assign;
-        struct{Operand result, op1, op2;} binop;
-        string funcName;
-        int LabelNum;       //number of label
-    }stuff;
+    Operand result, op1, op2;
+    string produceCode();
+};
+
+class FuncDecCode:public InterCode{
+public:
+    FuncDecCode(IRtype type, string *funcName);
+    const string getFuncName(){return funcName;}
+
+private:
+    string funcName;
+    string produceCode();
+};
+
+class LabelCode:public InterCode{
+public:
+    LabelCode(IRtype type, int labelNo);
+    const int getLabelNo(){return labelNo;}
+
+private:
+    int labelNo;
+    string produceCode();
+};
+
+class GotoCode:public InterCode{
+public:
+    GotoCode(IRtype type, int dstLabel);
+    const int getDst(){return dstLabel;}
+
+private:
+    int dstLabel;
+    string produceCode();
+};
+
+class CondCode:public InterCode{
+public:
+    enum Relop{
+        LE, LEQ, GE, GEQ, EQ, AND, OR, NE
+    };
+    CondCode(IRtype type, Operand *op1, Operand *op2, Relop relop, int dstLabel);
+    Operand getOp1(){return op1;}
+    Operand getOp2(){return op2;}
+    Relop getRelop(){return relop;}
+    int getDst(){return dstLabel;}
+private:
+    Operand op1, op2;
+    Relop relop;
+    int dstLabel;
+};
+
+class RetCode:public InterCode{
+public:
+    RetCode(IRtype type, Operand* retVal);
+    const Operand getRetVal(){return retVal;}
+private:
+    Operand retVal;
+    string produceCode();
+};
+
+class DecCode:public InterCode{
+public:
+    DecCode(IRtype type, Operand* OP, int size);
+    const Operand getOp(){return OP;}
+    const int getSize(){return size;}
+private:
+    Operand OP;
+    int size;
+    string produceCode();
+};
+
+class ArgCode:public InterCode{
+public:
+    ArgCode(IRtype type, Operand* arg);
+    const Operand getArg(){return arg;}
+private:
+    Operand arg;
+    string produceCode();
+};
+
+
+class CallCode:public InterCode{
+public:
+    CallCode(IRtype type, Operand *retVal, string funcName);
+    const Operand getRetVal(){return retVal;}
+    const string getFuncName(){return funcName;}
+
+private:
+    Operand retVal;
+    string funcName;
+    string produceCode();
+};
+
+
+class ParamCode:public InterCode{
+public:
+    ParamCode(IRtype type, Operand *param);
+    const Operand getParam(){return param;}
+
+private:
+    Operand param;
+    string produceCode();
+};
+
+
+class ReadCode:public InterCode{
+public:
+    ReadCode(IRtype type, Operand *OP);
+    const Operand getReadVal(){return val;}
+
+private:
+    Operand val;
+    string produceCode();
+};
+
+
+class WriteCode:public InterCode{
+public:
+    WriteCode(IRtype type, Operand *val);
+    const Operand getWriteVal(){return val;}
+
+private:
+    Operand val;
+    string produceCode();
+};
+
+
+class IRlist{
+public:
+    void printCodeList();
+private:
+    list<InterCode*> IRCodeList;
 };
---
+++ IR/src/InterCode.cpp
@@ -0,0 +1,151 @@
+#include"InterCode.h"
+#include<iostream>
+#include<sstream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::stringstream;
+/**********************class Operand****************************/
+int Operand::tempVarNum = 0;
+
+string Operand::produceName(){
+    string temp;
+    if(type == VARIABLE || type == ADRESS){
+        temp.append("v");
+        stringstream *ss = new stringstream;
+        string *strNo = new string;
+        *ss<<this->No;
+        *ss>>*strNo;
+        temp.append(*strNo);
+        delete ss;
+        delete strNo;
+    }
+    else if(type == TEMPVARIABLE || type == TEMPADRESS){
+        temp.append("t");
+        this->tempVarNum+= 1;
+        stringstream *ss;
+        string *strNo = new string;
+        *ss <<this->tempVarNum;
+        *ss >>*strNo;
+        temp.append(*strNo);
+        delete ss;
+        delete strNo;
+    }
+    else{
+        temp.append("#");
+        string *strVal = new string;
+        stringstream* ss = new stringstream;
+        if(type == ICONSTANT){
+            *ss << value.IVal;
+            *ss >> *strVal;
+        }
+        else{
+            *ss <<value.FVal;
+            *ss >>*strVal;
+        }
+        temp.append(*strVal);
+        delete strVal;
+        delete ss;
+    }
+    return temp;
+}
+
+
+
+/***********************class AssignCode*******************************/
+
+AssignCode::AssignCode(IRtype type, Operand* left, Operand *right):InterCode(type),\
+left(*left), right(*right){
+    this->code = produceCode();
+}
+
+string AssignCode::produceCode(){
+    string tempCode;
+    if(this->type == ASSIGN){
+        tempCode.append(left.getName());
+        tempCode.append(" := ");
+        tempCode.append(right.getName());
+    }
+    else if(this->type == ASSIGNLOC){
+        tempCode.append(left.getName());
+        tempCode.append(" := &");
+        tempCode.append(right.getName());
+    }
+    else if(this->type == ASSIGNFROMLOC){
+        tempCode.append(left.getName());
+        tempCode.append(" := *");
+        tempCode.append(right.getName());
+    }
+    else{
+        tempCode.append("*");
+        tempCode.append(left.getName());
+        tempCode.append(" := ");
+        tempCode.append(right.getName());
+    }
+    return tempCode;
+}
+
+
+
+/**********************class BinopCode**************/
+BinopCode::BinopCode(IRtype type, Operand* result, Operand *op1, Operand *op2):\
+InterCode(type), result(*result), op1(*op1), op2(*op2){
+    this->code = produceCode();    
+}
+
+string BinopCode::produceCode(){
+    string temp;
+    temp.append(this->result.getName());
+    temp.append(" := ");
+    temp.append(this->op1.getName());
+    if(this->type == PLUS)
+        temp.append(" + ");
+    else if(this->type == SUB)
+        temp.append(" - ");
+    else if(this->type == MUL)
+        temp.append(" * ");
+    else
+        temp.append(" / ");
+    temp.append(this->op2.getName());
+    return temp;
+}
+
+
+
+/*************************class FuncDecCode***************************/
+FuncDecCode::FuncDecCode(IRtype type, string *funcName):\
+InterCode(type), funcName(*funcName){
+    this->code = produceCode();
+}
+
+string FuncDecCode::produceCode(){
+    string temp;
+    temp.append("FUNCTION ");
+    temp.append(funcName);
+    temp.append(" :");
+}
+
+
+
+
+/****************************class LabelCode*****************************/
+LabelCode::LabelCode(IRtype type, int labelNo):\
+InterCode(type), labelNo(labelNo){
+    this->code = produceCode();
+}
+
+string LabelCode::produceCode(){
+    string temp;
+    stringstream* ss = new stringstream;
+    string strNo;
+    *ss << labelNo;
+    *ss >> strNo;
+    temp.append("LABEL ");
+    temp.append(strNo);
+    temp.append(" :");
+    return temp;
+}
+
+
+
+/***************************************************************/
